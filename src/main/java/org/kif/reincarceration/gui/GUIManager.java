eee5e2042d36613f3eed5a6ae70df097370d33d9
MidNoon <MidNoon@users.noreply.github.com>
2024-07-24 21:44:06 -0500

    add entry fee disable on start cycle button

--- src/main/java/org/kif/reincarceration/gui/GUIManager.java
+++ src/main/java/org/kif/reincarceration/gui/GUIManager.java
@@ -74,7 +74,7 @@ public class GUIManager {
             boolean isMaxRank = configManager.isMaxRank(currentRank);
 
             if (!inCycle && player.hasPermission("reincarceration.startcycle")) {
-                inventory.setItem(21, createGuiItem(Material.IRON_BARS, ChatColor.GREEN + "Start Cycle", "Begin a new reincarceration cycle"));
+                inventory.setItem(21, createGuiItem(Material.IRON_BARS, ChatColor.GREEN + "Start Cycle", "Begin a new reincarceration cycle ($" + configManager.getEntryFee() + ")"));
             } else {
                 inventory.setItem(21, createDisabledGuiItem(Material.BARRIER, ChatColor.GRAY + "Start Cycle", "You can't start a new cycle now"));
             }
